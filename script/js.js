1900a9b6c8e53f315302f1dc1c3cb92230cd193f
krupanjac <arsen.djurdjev@gmail.com>
2024-12-30 11:58:56 +0100

    Add SVG assets and update navigation with new logo; enhance styling and functionality

--- script/js.js
+++ script/js.js
@@ -18,13 +18,24 @@ const navMenu = document.getElementById("nav-menu");
 
 window.addEventListener("scroll", () => {
   const currentScrollY = window.scrollY;
+  // add a bool
+    
 
   // If the user scrolls up, show the nav menu
   if (currentScrollY < lastScrollY) {
       navMenu.style.transform = `translateY(+${currentScrollY}px)`; // Move the nav up as you scroll down
+        //z-index to 9999 to make sure it's on top
+        navMenu.style.zIndex = "999";
+
+
+
+
   } else {
       // If the user scrolls down, hide the nav menu
       navMenu.style.transform = "translateY(-100%)";
+        //z-index to 0 to make sure it's on bottom
+        navMenu.style.zIndex = "0";
+        
   }
 
   // Update the last scroll position
